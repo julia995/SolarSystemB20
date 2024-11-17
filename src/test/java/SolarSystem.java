6709c15a22eaf81d56359e7266ccbbf761fe6770
julia995 <julia09.matyusheva@gmail.com>
2024-11-17 12:55:08 -0500

    second planet

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -5,5 +5,9 @@ public class SolarSystem {
         solar.Sun="1";
         solar.Moon = "1";
         solar.Planet = "2";
+
+        planet2 nobel = new planet2();
+        nobel.temp = "5464 f";
+        nobel.color = "black";
     }
 }
